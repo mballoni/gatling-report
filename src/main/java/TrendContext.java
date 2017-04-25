b7ea265155ef56783be3637bcae0d71222e4a802
mballoni <mballoni@ciandt.com>
2017-04-25 11:57:09 -0300

    Using index instead of timestamp on X axis

--- src/main/java/TrendContext.java
+++ src/main/java/TrendContext.java
@@ -15,82 +15,80 @@
  *     Benoit Delbosc
  */
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TrendContext {
 
-    String scenario;
-    TrendStat all = new TrendStat();
-    List<TrendStat> requests = new ArrayList<>();
-    List<String> scripts;
+  String scenario;
+  TrendStat all = new TrendStat();
+  List<TrendStat> requests = new ArrayList<>();
+  List<String> scripts;
 
 
-    class TrendStat {
-        String name;
-        Integer indice;
-        List<String> xvalues = new ArrayList<>();
-        List<Double> yvalues = new ArrayList<>();
-        List<Long> yerrors = new ArrayList<>();
-        List<Double> rps = new ArrayList<>();
+  class TrendStat {
+    String name;
+    Integer indice;
+    List<String> xvalues = new ArrayList<>();
+    List<Double> yvalues = new ArrayList<>();
+    List<Long> yerrors = new ArrayList<>();
+    List<Double> rps = new ArrayList<>();
 
-        public void add(RequestStat stat) {
-            if (stat == null) {
-                xvalues.add(null);
-                yvalues.add(null);
-                yerrors.add(null);
-                rps.add(null);
-            } else {
-                name = stat.request;
-                indice = stat.indice;
-                xvalues.add(String.format("'%s'", stat.startDate));
-                yvalues.add(stat.avg);
-                yerrors.add(stat.stddev);
-                rps.add(stat.rps);
-            }
-        }
+    public void add(RequestStat stat, Integer count) {
+      if (stat == null) {
+        xvalues.add(null);
+        yvalues.add(null);
+        yerrors.add(null);
+        rps.add(null);
+      } else {
+        name = stat.request;
+        indice = stat.indice;
+        xvalues.add(String.valueOf(count));
+        yvalues.add(stat.avg);
+        yerrors.add(stat.stddev);
+        rps.add(stat.rps);
+      }
     }
+  }
 
-    public TrendContext(List<SimulationContext> stats) {
-        Set<String> names = new HashSet<>();
-        List<String> requestNames = getRequestListSorted(stats.get(0));
-        Collections.reverse(requestNames);
-        for (String requestName : requestNames) {
-            requests.add(new TrendStat());
-        }
-        ArrayList<SimulationContext> orderedStats = new ArrayList<SimulationContext>(stats);
-        Collections.sort(orderedStats, (a, b) -> (int) (a.simStat.start - b.simStat.start));
-        for (SimulationContext simStat : orderedStats) {
-            names.add(simStat.simulationName);
-            all.add(simStat.simStat);
-            for (int i = 0; i < requestNames.size(); i++) {
-                String name = requestNames.get(i);
-                RequestStat reqStat = simStat.reqStats.get(name);
-                requests.get(i).add(reqStat);
-            }
-        }
-        scenario = String.join(" ", names);
+  public TrendContext(List<SimulationContext> stats) {
+    Set<String> names = new HashSet<>();
+    List<String> requestNames = getRequestListSorted(stats.get(0));
+    Collections.reverse(requestNames);
+    for (String requestName : requestNames) {
+      requests.add(new TrendStat());
     }
+    ArrayList<SimulationContext> orderedStats = new ArrayList<>(stats);
+    Collections.sort(orderedStats, (a, b) -> (int) (a.simStat.start - b.simStat.start));
+    int count = 1;
+    for (SimulationContext simStat : orderedStats) {
+      names.add(simStat.simulationName);
+      all.add(simStat.simStat, count);
+      for (int i = 0; i < requestNames.size(); i++) {
+        String name = requestNames.get(i);
+        RequestStat reqStat = simStat.reqStats.get(name);
+        requests.get(i).add(reqStat, null);
+      }
+      count++;
+    }
+    scenario = String.join(" ", names);
+  }
 
-    public TrendContext setScripts(List<String> scripts) {
-        this.scripts = scripts;
-        return this;
-    }
+  public TrendContext setScripts(List<String> scripts) {
+    this.scripts = scripts;
+    return this;
+  }
 
-    private List<String> getRequestListSorted(SimulationContext stat) {
-        return stat.getRequests().stream().map(s -> s.request).collect(Collectors.toList());
-    }
+  private List<String> getRequestListSorted(SimulationContext stat) {
+    return stat.getRequests().stream().map(s -> s.request).collect(Collectors.toList());
+  }
 
-    public TrendStat getRef() {
-        return requests.get(0);
-    }
+  public TrendStat getRef() {
+    return requests.get(0);
+  }
 
-    public TrendStat getChallenger() {
-        return requests.get(1);
-    }
+  public TrendStat getChallenger() {
+    return requests.get(1);
+  }
 
 }
